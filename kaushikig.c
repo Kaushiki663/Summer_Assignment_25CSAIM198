ffd01366d9ea730b712c054be9f1c953b720acea
Kaushiki663 <kaushikig385@gmail.com>
2026-06-10 20:42:19 +0530

    day 2 uploaded

---
+++ kaushikig.c
@@ -0,0 +1,10 @@
+Q1	#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        sum += i;
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+    return 0;
+}
