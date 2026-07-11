dac8195ba684af6b4cb4e12cebeed1a5eea843b6
KAUSHIKI <kaushikig385@gmail.com>
2026-07-11 22:17:40 +0530

    Add files via upload

---
+++ day6.3.c
@@ -0,0 +1,17 @@
+//Question:Write a program to count set bits in a number.
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        if (num & 1)
+            count++;
+        num >>= 1;
+    }
+
+    printf("Number of set bits: %d", count);
+
+    return 0;
+}
