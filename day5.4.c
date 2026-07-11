054f576d680ac5a0e574e8de1f7d75b82d6c6112
KAUSHIKI <kaushikig385@gmail.com>
2026-07-11 22:03:38 +0530

    Add files via upload

---
+++ day5.4.c
@@ -0,0 +1,27 @@
+//Question: Write a program to Find largest prime factor.
+
+int main() {
+    int num, i, largestPrime = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num % 2 == 0) {
+        largestPrime = 2;
+        num /= 2;
+    }
+
+    for (i = 3; i * i <= num; i += 2) {
+        while (num % i == 0) {
+            largestPrime = i;
+            num /= i;
+        }
+    }
+
+    if (num > 2)
+        largestPrime = num;
+
+    printf("Largest Prime Factor = %d", largestPrime);
+
+    return 0;
+}
