697cc60093702b6b28a0fcca0e53f0732927f2ac
KAUSHIKI <kaushikig385@gmail.com>
2026-07-12 14:03:00 +0530

    Add files via upload

---
+++ day13.1.c
@@ -0,0 +1,20 @@
+//Question: Write a program to Input and display array.
+
+int main() {
+    int arr[10], i, n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The elements of the array are: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
