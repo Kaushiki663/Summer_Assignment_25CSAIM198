cf41f2a6f4fd255f3897fb3b54a5aba7436528b3
KAUSHIKI <kaushikig385@gmail.com>
2026-07-12 12:36:50 +0530

    Add files via upload

---
+++ day10.2.c
@@ -0,0 +1,17 @@
+//Question:Write a program to Print reverse pyramid.
+
+int main() {
+    int i, j;
+
+    for(i = 5; i >= 1; i--) {
+        for(j = 1; j <= 5 - i; j++)
+            printf(" ");
+
+        for(j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+
+        printf("\n");
+    }
+
+    return 0;
+}
